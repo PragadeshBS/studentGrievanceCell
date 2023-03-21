86b51eef8805515e2699faa421a8efcf726c45b5
Pragadesh BS <pragadeshbs@protonmail.com>
2023-03-21 07:11:48 +0530

    fix typo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import StaffLogin from "./pages/Auth/StaffLogin";
-import StudentLogin from "./pages/Auth/StudentLogin";
+import StaffLogin from "./pages/auth/StaffLogin";
+import StudentLogin from "./pages/auth/StudentLogin";
 import SubmitGrievance from "./pages/grievance/SubmitGrievance";
 import ViewGrievance from "./pages/grievance/ViewGrievance";
 
