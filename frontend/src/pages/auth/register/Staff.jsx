d08c231aab0299f3a8a568ef61c5fab832da656e
Pragadesh BS <pragadeshbs@protonmail.com>
2023-04-27 21:58:52 +0530

    add ui for staff and student register

--- frontend/src/pages/auth/register/Staff.jsx
+++ frontend/src/pages/auth/register/Staff.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuthDispatch } from "../../../context/AuthContext";
+import ClipLoaderWithText from "../../../components/loaders/ClipLoaderWithText";
 
 const StaffRegister = () => {
   const authDispatch = useAuthDispatch();
@@ -51,106 +52,192 @@ const StaffRegister = () => {
       });
   }, []);
   return (
-    <div>
-      <h1>Staff Register</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <input
-            placeholder="Staff ID"
-            {...register("staffId", {
-              required: "Staff ID is required",
-              minLength: {
-                value: 3,
-                message: "Staff ID must be at least 3 characters",
-              },
-            })}
-          />
-          {errors.staffId && <p role="alert">{errors.staffId?.message}</p>}
-        </div>
-        <div>
-          <input
-            placeholder="Name"
-            {...register("name", {
-              required: "Name is required",
-            })}
-          />
-          {errors.name && <p role="alert">{errors.name?.message}</p>}
-        </div>
-        <div>
-          <select
-            {...register("department", {
-              required: "Department is required",
-            })}
-          >
-            <option value="">Select Department</option>
-            {departments.map((department) => (
-              <option key={department._id} value={department._id}>
-                {department.name}
-              </option>
-            ))}
-          </select>
-          {errors.department && (
-            <p role="alert">{errors.department?.message}</p>
-          )}
-        </div>
-        <div>
-          <input
-            placeholder="Designation"
-            {...register("designation", {
-              required: "Designation is required",
-            })}
-          />
-          {errors.designation && (
-            <p role="alert">{errors.designation?.message}</p>
-          )}
-        </div>
-        <div>
-          <input
-            placeholder="Email"
-            {...register("email", {
-              required: "Email is required",
-            })}
-          />
-          {errors.email && <p role="alert">{errors.email?.message}</p>}
-        </div>
-        <div>
-          <input
-            placeholder="Phone"
-            {...register("phone", {
-              required: "Phone is required",
-            })}
-          />
-          {errors.phone && <p role="alert">{errors.phone?.message}</p>}
-        </div>
-        <div>
-          <input
-            placeholder="Password"
-            type="password"
-            {...register("password", {
-              required: "Password is required",
-            })}
-          />
-          {errors.password && <p role="alert">{errors.password?.message}</p>}
-        </div>
-        <div>
-          <input
-            placeholder="Confirm Password"
-            type="password"
-            {...register("confirmPassword", {
-              required: "Confirm Password is required",
-            })}
-          />
-          {errors.confirmPassword && (
-            <p role="alert">{errors.confirmPassword?.message}</p>
-          )}
-        </div>
-        {errorMsg && <div>{errorMsg}</div>}
-        <div>
-          <button type="submit" disabled={loading}>
-            {loading ? "Loading..." : "Register"}
-          </button>
-        </div>
-      </form>
+    <div className="container mx-auto flex justify-center">
+      <div className="flex-grow max-w-3xl px-3">
+        <h1 className="text-4xl font-extrabold my-5 text-center">
+          Staff Register
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">Staff ID</label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.staffId ? " border-red-500 dark:border-red-500" : "")
+              }
+              placeholder="Staff ID"
+              {...register("staffId", {
+                required: "Staff ID is required",
+                minLength: {
+                  value: 3,
+                  message: "Staff ID must be at least 3 characters",
+                },
+              })}
+            />
+            {errors.staffId && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.staffId?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">Name</label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.name ? " border-red-500 dark:border-red-500" : "")
+              }
+              placeholder="Name"
+              {...register("name", {
+                required: "Name is required",
+              })}
+            />
+            {errors.name && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.name?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">Department</label>
+            <select
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.department ? " border-red-500 dark:border-red-500" : "")
+              }
+              {...register("department", {
+                required: "Department is required",
+              })}
+            >
+              <option value="">Select Department</option>
+              {departments.map((department) => (
+                <option key={department._id} value={department._id}>
+                  {department.name}
+                </option>
+              ))}
+            </select>
+            {errors.department && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.department?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">
+              Designation
+            </label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.designation
+                  ? " border-red-500 dark:border-red-500"
+                  : "")
+              }
+              placeholder="Designation"
+              {...register("designation", {
+                required: "Designation is required",
+              })}
+            />
+            {errors.designation && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.designation?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">Email</label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.email ? " border-red-500 dark:border-red-500" : "")
+              }
+              placeholder="Email"
+              {...register("email", {
+                required: "Email is required",
+              })}
+            />
+            {errors.email && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.email?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">Phone</label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.phone ? " border-red-500 dark:border-red-500" : "")
+              }
+              placeholder="Phone"
+              {...register("phone", {
+                required: "Phone is required",
+              })}
+            />
+            {errors.phone && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.phone?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">Password</label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.password ? " border-red-500 dark:border-red-500" : "")
+              }
+              placeholder="Password"
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+              })}
+            />
+            {errors.password && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.password?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">
+              Confirm Password
+            </label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.confirmPassword
+                  ? " border-red-500 dark:border-red-500"
+                  : "")
+              }
+              placeholder="Confirm Password"
+              type="password"
+              {...register("confirmPassword", {
+                required: "Confirm Password is required",
+              })}
+            />
+            {errors.confirmPassword && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.confirmPassword?.message}
+              </p>
+            )}
+          </div>
+          {errorMsg && <div>{errorMsg}</div>}
+          <div className="mb-6">
+            {loading ? (
+              <ClipLoaderWithText text="Registering..." />
+            ) : (
+              <button
+                type="submit"
+                disabled={loading}
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Register
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/auth/register/Student.jsx
+++ frontend/src/pages/auth/register/Student.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuthDispatch } from "../../../context/AuthContext";
+import ClipLoaderWithText from "../../../components/loaders/ClipLoaderWithText";
 
 const StudentRegister = () => {
   const authDispatch = useAuthDispatch();
@@ -49,109 +50,184 @@ const StudentRegister = () => {
       });
   }, []);
   return (
-    <div>
-      <h1>Student Register</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <input
-            placeholder="Register No."
-            {...register("registerNo", {
-              required: "Register No. is required",
-              minLength: {
-                value: 3,
-                message: "Register No. must be at least 3 characters",
-              },
-            })}
-          />
-          {errors.registerNo && (
-            <p role="alert">{errors.registerNo?.message}</p>
-          )}
-        </div>
-        <div>
-          <input
-            placeholder="Name"
-            {...register("name", {
-              required: "Name is required",
-            })}
-          />
-          {errors.name && <p role="alert">{errors.name?.message}</p>}
-        </div>
-        <div>
-          <input
-            placeholder="Email"
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /^\S+@\S+$/i,
-                message: "Invalid email address",
-              },
-            })}
-          />
-          {errors.email && <p role="alert">{errors.email?.message}</p>}
-        </div>
-        <div>
-          <input
-            placeholder="Phone"
-            {...register("phone", {
-              required: "Phone is required",
-            })}
-          />
-          {errors.phone && <p role="alert">{errors.phone?.message}</p>}
-        </div>
-        <div>
-          <select
-            {...register("department", {
-              required: "Department is required",
-            })}
-          >
-            <option value="">Select Department</option>
-            {departments.map((department) => (
-              <option key={department._id} value={department._id}>
-                {department.name}
-              </option>
-            ))}
-          </select>
-          {errors.department && (
-            <p role="alert">{errors.department?.message}</p>
-          )}
-        </div>
-        <div>
-          <input
-            placeholder="Password"
-            type="password"
-            {...register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 6,
-                message: "Password must be at least 6 characters",
-              },
-            })}
-          />
-          {errors.password && <p role="alert">{errors.password?.message}</p>}
-        </div>
-        <div>
-          <input
-            placeholder="Confirm Password"
-            type="password"
-            {...register("confirmPassword", {
-              required: "Confirm Password is required",
-              minLength: {
-                value: 6,
-                message: "Confirm Password must be at least 6 characters",
-              },
-            })}
-          />
-          {errors.confirmPassword && (
-            <p role="alert">{errors.confirmPassword?.message}</p>
-          )}
-        </div>
-        {errorMsg && <p role="alert">{errorMsg}</p>}
-        <div>
-          <button type="submit" disabled={loading}>
-            {loading ? "Loading..." : "Register"}
-          </button>
-        </div>
-      </form>
+    <div className="container mx-auto flex justify-center">
+      <div className="flex-grow max-w-3xl px-3">
+        <h1 className="text-4xl font-extrabold my-5 text-center">
+          Student Register
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">
+              Register No.
+            </label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.registerNo ? " border-red-500 dark:border-red-500" : "")
+              }
+              placeholder="Register No."
+              {...register("registerNo", {
+                required: "Register No. is required",
+                minLength: {
+                  value: 3,
+                  message: "Register No. must be at least 3 characters",
+                },
+              })}
+            />
+            {errors.registerNo && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.registerNo?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">Name</label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.name ? " border-red-500 dark:border-red-500" : "")
+              }
+              placeholder="Name"
+              {...register("name", {
+                required: "Name is required",
+              })}
+            />
+            {errors.name && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.name?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">Email</label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.email ? " border-red-500 dark:border-red-500" : "")
+              }
+              placeholder="Email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: "Invalid email address",
+                },
+              })}
+            />
+            {errors.email && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.email?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">Phone</label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.phone ? " border-red-500 dark:border-red-500" : "")
+              }
+              placeholder="Phone"
+              {...register("phone", {
+                required: "Phone is required",
+              })}
+            />
+            {errors.phone && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.phone?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">Department</label>
+            <select
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.department ? " border-red-500 dark:border-red-500" : "")
+              }
+              {...register("department", {
+                required: "Department is required",
+              })}
+            >
+              <option value="">Select Department</option>
+              {departments.map((department) => (
+                <option key={department._id} value={department._id}>
+                  {department.name}
+                </option>
+              ))}
+            </select>
+            {errors.department && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.department?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">Password</label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.password ? " border-red-500 dark:border-red-500" : "")
+              }
+              placeholder="Password"
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters",
+                },
+              })}
+            />
+            {errors.password && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.password?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 text-xl font-medium">
+              Confirm Password
+            </label>
+            <input
+              className={
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" +
+                (errors.confirmPassword
+                  ? " border-red-500 dark:border-red-500"
+                  : "")
+              }
+              placeholder="Confirm Password"
+              type="password"
+              {...register("confirmPassword", {
+                required: "Confirm Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Confirm Password must be at least 6 characters",
+                },
+              })}
+            />
+            {errors.confirmPassword && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.confirmPassword?.message}
+              </p>
+            )}
+          </div>
+          {errorMsg && <p role="alert">{errorMsg}</p>}
+          <div className="mb-6">
+            {loading ? (
+              <ClipLoaderWithText text={"Registering"} />
+            ) : (
+              <button
+                type="submit"
+                disabled={loading}
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Register
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
